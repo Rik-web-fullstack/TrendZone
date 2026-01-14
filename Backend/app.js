afe1fc045a69c06bc22664260c935d0d0f1fb107
Rik Maity <rikmaity522@gmail.com>
2026-01-14 10:44:40 +0530

    minor-changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,5 @@
-const db = require("./config/db");
 require("dotenv").config();
+const db = require("./config/db");
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
