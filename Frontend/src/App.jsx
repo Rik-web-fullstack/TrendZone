13bc8c939ae6a150a406ffb23b1c376402e9a7ad
Rik Maity <rikmaity522@gmail.com>
2026-01-16 22:36:29 +0530

    admin

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       <Routes>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/adminPanel' element={<AdminPanel />}></Route>
+        <Route path='/admin' element={<AdminPanel />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/cart' element={<Cart />}></Route>
         <Route path='/wishlist' element={<Wishlist />}></Route>
